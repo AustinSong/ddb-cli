937a6c66c8fc9f55209aa4be0bf2142d65b68a25
Austin Song <austins89@gmail.com>
2017-02-19 22:07:04 -0800

    add experimental ssl cert config

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-DDB_HOST =  'https://10.0.2.2:8081'
+# DDB_HOST =  'https://10.0.2.2:8081'
+DDB_HOST =  'https://localhost:8081'
 DDB_KEY = 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=='
+SSL_CERT = "C:\Users\austi\Desktop"
--- pyddb.py
+++ pyddb.py
@@ -3,6 +3,8 @@ import click
 import config
 
 import pydocumentdb.document_client as document_client
+import pydocumentdb.retry_options as retry_opts
+import pydocumentdb.documents as documents
 
 # Creates the DocumentDB client connection.
 @click.group()
@@ -10,7 +12,7 @@ import pydocumentdb.document_client as document_client
 def main(ctx):
     click.echo("Creating client")
     ctx.obj = document_client.DocumentClient(config.DDB_HOST,
-                                             {'masterKey':config.DDB_KEY})
+                                             {'masterKey':config.DDB_KEY}, createConnectionPolicy())
 
 # Creates a database with the name as the id.  Utilizes the client from
 # main function.
@@ -24,5 +26,23 @@ def createdb(ctx, name):
     db = client.CreateDatabase({'id':name})
     click.echo('Created db with name: ' + name)
 
+def createConnectionPolicy():
+    conn_policy = documents.ConnectionPolicy
+    conn_policy.EnableEndpointDiscovery = 0
+    conn_policy.PreferredLocations = ['West US']
+    conn_policy.RetryOptions =  retry_opts.RetryOptions(3)
+    conn_policy.RequestTimeout = 3000
+    conn_policy.SSLConfiguration = createSSLConfiguration()
+    return conn_policy
+
+
+def createSSLConfiguration():
+    ssl_config = documents.SSLConfiguration()
+    ssl_config.SSLCaCerts = config.SSL_CERT
+    ssl_config.SSLKeyFile = config.SSL_CERT
+    ssl_config.SSLCertFile = config.SSL_CERT
+    return ssl_config
+
 if __name__=='__main__':
     main()
+
